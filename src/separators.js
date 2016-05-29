73635f0736dad612719af2591dfb9cd155eb8505
yanruoxinb <yanruoxinb@users.noreply.github.com>
2016-05-29 12:28:15 +0800

    Update separators.js

--- src/separators.js
+++ src/separators.js
@@ -1,11 +1,20 @@
 'use strict';
 
 function thousands_separators(num) {
-	var a = RegExp.$1,b = RegExp.$2,c = RegExp.$3; 
-	var re = new RegExp().compile("(\\d)(\\d{3})(,|$)"); 
-	while(re.test(b)){ 
- 		b=b.replace(re,"$1,$2$3"); 
- 	} 
-	return a +""+ b +""+ c; 
+	 var num = (num || 0).toString(),
+	  re = /\d{3}$/,
+	  result = '';
+    while ( re.test(num) ) {
+        result = RegExp.lastMatch + result;
+        if (num !== RegExp.lastMatch) {
+            result = ',' + result;
+            num = RegExp.leftContext;
+        } else {
+            num = '';
+            break;
+        }
+    }
+    if (num) { result = num + result; }
+    return result;
 }
 module.exports = thousands_separators;
