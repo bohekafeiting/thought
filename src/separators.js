0f014b63064e2d1fa913137d3ad6e8a4bb5fac9a
yanruoxinb <yanruoxinb@users.noreply.github.com>
2016-05-29 10:52:09 +0800

    Update separators.js

--- src/separators.js
+++ src/separators.js
@@ -1,13 +1,12 @@
 'use strict';
 
 function thousands_separators(num) {
-var tex=return num && (num
+return num && (num
     .toString().indexOf('.') != -1 ? num.toString().replace(/(\d)(?=(\d{3})+\.)/g, function($0, $1) {
       return $1 + ",";
     }) : num.toString().replace(/(\d)(?=(\d{3}))/g, function($0, $1) {
       return $1 + ",";
     }));
-  return tex.toString();
 }
 
 module.exports = thousands_separators;
