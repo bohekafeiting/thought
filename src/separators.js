2c1adf006227c2073a0737088f8d2d1179f876c5
yanruoxinb <yanruoxinb@users.noreply.github.com>
2016-05-29 13:02:49 +0800

    Update separators.js

--- src/separators.js
+++ src/separators.js
@@ -1,20 +1,20 @@
 'use strict';
 
 function thousands_separators(num) {
-	 var num = (num || 0).toString(),
-	  re = /\d{3}$/,
-	  result = '';
-    while ( re.test(num) ) {
-        result = RegExp.lastMatch + result;
-        if (num !== RegExp.lastMatch) {
-            result = ',' + result;
-            num = RegExp.leftContext;
-        } else {
-            num = '';
-            break;
-        }
-    }
-    if (num) { result = num + result; }
-    return result;
+	 var numString = num.toString(),
+	flag = numString.indexOf("."),
+	_length = numString.length,
+	_point = "",
+	_int = "";
+	if (/\d+/.test(numString) && !isNaN(num)) {
+	if (flag != -1) {
+	_point = numString.substring(flag, _length);
+	_int = numString.substring(0, flag).split('').reverse().join('').replace(/(\d{3})/g, '$1,').replace(/\,$/, '').split('').reverse().join('');
+	} 
+	else {
+	_int = numString.split('').reverse().join('').replace(/(\d{3})/g, '$1,').replace(/\,$/, '').split('').reverse().join('');
+	}
+	return num = _int + _point;
+	} 
 }
 module.exports = thousands_separators;
